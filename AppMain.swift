e110c5c595113951d786c976d11a7c9283bf499e
Aln-gp <alnofaie.gp@gmail.com>
2026-06-27 20:39:32 +0300

    Initialize CarPlaySuperApp with AppDelegate

---
+++ AppMain.swift
@@ -0,0 +1,24 @@
+import SwiftUI
+
+@main
+struct CarPlaySuperApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
+    var body: some Scene {
+        WindowGroup {
+            PhoneMainView()
+        }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        return true
+    }
+    
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        let config = UISceneConfiguration(name: "CarPlay", sessionRole: connectingSceneSession.role)
+        config.delegateClass = CarPlaySceneDelegate.self
+        return config
+    }
+}
