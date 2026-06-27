ba8683a3cdd4ef84c5af3f0d85250cdea5dedfec
Aln-gp <alnofaie.gp@gmail.com>
2026-06-27 21:00:16 +0300

    Refactor CarPlaySceneDelegate for CPWindow usage
    Updated CarPlaySceneDelegate to use CPWindow and modified app grid display logic.

--- CarPlaySceneDelegate.swift
+++ CarPlaySceneDelegate.swift
@@ -5,9 +5,8 @@ import WebKit
 class CarPlaySceneDelegate: UIResponder, CPTemplateApplicationSceneDelegate {
     
     var interfaceController: CPInterfaceController?
-    var carWindow: UIWindow?
+    var carWindow: CPWindow? // تعديل: استخدام نوع النافذة المخصصة للكاربلاي حقيقية
     var carWebView: WKWebView?
-    var syncTimer: Timer?
     
     let allApps = [
         CarAppItem(id: "yt", name: "YouTube", url: "https://www.youtube.com", icon: "play.rectangle.fill"),
@@ -16,52 +15,60 @@ class CarPlaySceneDelegate: UIResponder, CPTemplateApplicationSceneDelegate {
         CarAppItem(id: "tw", name: "X / Twitter", url: "https://www.x.com", icon: "message.fill")
     ]
     
+    // الدالة الرسمية التي تطلقها السيارة عند الاتصال الحقيقي
     func templateApplicationScene(_ templateApplicationScene: CPTemplateApplicationScene, didConnect interfaceController: CPInterfaceController, to window: CPWindow) {
         self.interfaceController = interfaceController
         self.carWindow = window
         
-        let config = WKWebViewConfiguration()
-        config.allowsInlineMediaPlayback = true
-        carWebView = WKWebView(frame: window.bounds, configuration: config)
-        carWebView?.customUserAgent = "Mozilla/5.0 (iPad; CPU OS 17_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Mobile/15E148 Safari/604.1"
-        
-        syncTimer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true) { _ in
-            self.updateCarScreen()
-        }
+        // إظهار شبكة التطبيقات فوراً عند تشغيل شاشة السيارة
+        showAppGrid()
     }
     
-    func updateCarScreen() {
-        let isMirroring = UserDefaults.standard.bool(forKey: "IsScreenMirroringActive")
+    func showAppGrid() {
+        let savedIDs = UserDefaults.standard.stringArray(forKey: "CarPlayEnabledApps") ?? ["yt", "tt", "go", "tw"]
+        let activeApps = allApps.filter { savedIDs.contains($0.id) }
         
-        if isMirroring {
-            launchUrlInCar(urlStr: "http://127.0.0.1:8080/stream")
-        } else {
-            let savedIDs = UserDefaults.standard.stringArray(forKey: "CarPlayEnabledApps") ?? []
-            let activeApps = allApps.filter { savedIDs.contains($0.id) }
-            
-            var gridButtons: [CPGridButton] = []
-            for app in activeApps {
-                let btn = CPGridButton(titleVariants: [app.name], image: UIImage(systemName: app.icon)!) { _ in
-                    self.launchUrlInCar(urlStr: app.url)
-                }
-                gridButtons.append(btn)
+        var gridButtons: [CPGridButton] = []
+        for app in activeApps {
+            // عند الضغط على أي تطبيق، يتم فتح الرابط الحقيقي فوراً داخل شاشة السيارة
+            let btn = CPGridButton(titleVariants: [app.name], image: UIImage(systemName: app.icon)!) { _ in
+                self.launchUrlInCar(urlStr: app.url)
             }
-            
-            let gridTemplate = CPGridTemplate(title: "تطبيقاتي المضافة", gridButtons: gridButtons)
-            self.interfaceController?.setRootTemplate(gridTemplate, animated: true)
+            gridButtons.append(btn)
         }
+        
+        let gridTemplate = CPGridTemplate(title: "تطبيقات يزن الحقيقية", gridButtons: gridButtons)
+        self.interfaceController?.setRootTemplate(gridTemplate, animated: true)
     }
     
     func launchUrlInCar(urlStr: String) {
-        guard let window = carWindow, let web = carWebView else { return }
-        if web.url?.absoluteString != urlStr {
-            web.load(URLRequest(url: URL(string: urlStr)!))
+        guard let window = carWindow else { return }
+        
+        // تنظيف الشاشة من أي متصفح قديم
+        carWebView?.removeFromSuperview()
+        window.subviews.forEach { $0.removeFromSuperview() }
+        
+        // إعداد المتصفح الحقيقي للسيارة مع تخطي قيود أبل للميديا
+        let config = WKWebViewConfiguration()
+        config.allowsInlineMediaPlayback = true
+        config.mediaTypesRequiringUserActionForPlayback = []
+        
+        // إجبار المتصفح على اتخاذ حجم شاشة السيارة بالكامل
+        let webViewFrame = CGRect(x: 0, y: 0, width: window.frame.width, height: window.frame.height)
+        carWebView = WKWebView(frame: webViewFrame, configuration: config)
+        
+        // تغيير الـ User Agent ليوهم المواقع أنه جهاز iPad حقيقي عشان تفتح النسخة الكاملة للموقع
+        carWebView?.customUserAgent = "Mozilla/5.0 (iPad; CPU OS 17_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Mobile/15E148 Safari/604.1"
+        
+        if let web = carWebView {
             window.addSubview(web)
+            web.load(URLRequest(url: URL(string: urlStr)!))
             
-            let homeBtn = UIButton(frame: CGRect(x: 10, y: 10, width: 45, height: 45))
+            // زر الهوم العائم للرجوع لقائمة التطبيقات
+            let homeBtn = UIButton(frame: CGRect(x: 15, y: 15, width: 45, height: 45))
             homeBtn.setImage(UIImage(systemName: "house.circle.fill"), for: .normal)
-            homeBtn.tintColor = .white
-            homeBtn.backgroundColor = UIColor.black.withAlphaComponent(0.5)
+            homeBtn.tintColor = .green
+            homeBtn.backgroundColor = UIColor.black.withAlphaComponent(0.6)
             homeBtn.layer.cornerRadius = 22.5
             homeBtn.addTarget(self, action: #selector(goBackToGrid), for: .touchUpInside)
             window.addSubview(homeBtn)
@@ -69,11 +76,14 @@ class CarPlaySceneDelegate: UIResponder, CPTemplateApplicationSceneDelegate {
     }
     
     @objc func goBackToGrid() {
+        // حذف المتصفح والرجوع للقائمة الأساسية
         carWebView?.removeFromSuperview()
+        carWindow?.subviews.forEach { $0.removeFromSuperview() }
+        showAppGrid()
     }
     
     func templateApplicationScene(_ templateApplicationScene: CPTemplateApplicationScene, didDisconnect interfaceController: CPInterfaceController) {
-        syncTimer?.invalidate()
         self.interfaceController = nil
+        self.carWindow = nil
     }
 }
