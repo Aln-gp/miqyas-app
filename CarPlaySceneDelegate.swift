e36c29fcc8e8755dff2b6ae1a4b1fcdc88f87235
Aln-gp <alnofaie.gp@gmail.com>
2026-06-27 21:05:25 +0300

    Refactor CarPlaySceneDelegate for clarity and fixes

--- CarPlaySceneDelegate.swift
+++ CarPlaySceneDelegate.swift
@@ -5,7 +5,7 @@ import WebKit
 class CarPlaySceneDelegate: UIResponder, CPTemplateApplicationSceneDelegate {
     
     var interfaceController: CPInterfaceController?
-    var carWindow: CPWindow? // تعديل: استخدام نوع النافذة المخصصة للكاربلاي حقيقية
+    var carWindow: CPWindow?
     var carWebView: WKWebView?
     
     let allApps = [
@@ -15,12 +15,9 @@ class CarPlaySceneDelegate: UIResponder, CPTemplateApplicationSceneDelegate {
         CarAppItem(id: "tw", name: "X / Twitter", url: "https://www.x.com", icon: "message.fill")
     ]
     
-    // الدالة الرسمية التي تطلقها السيارة عند الاتصال الحقيقي
     func templateApplicationScene(_ templateApplicationScene: CPTemplateApplicationScene, didConnect interfaceController: CPInterfaceController, to window: CPWindow) {
         self.interfaceController = interfaceController
         self.carWindow = window
-        
-        // إظهار شبكة التطبيقات فوراً عند تشغيل شاشة السيارة
         showAppGrid()
     }
     
@@ -30,7 +27,6 @@ class CarPlaySceneDelegate: UIResponder, CPTemplateApplicationSceneDelegate {
         
         var gridButtons: [CPGridButton] = []
         for app in activeApps {
-            // عند الضغط على أي تطبيق، يتم فتح الرابط الحقيقي فوراً داخل شاشة السيارة
             let btn = CPGridButton(titleVariants: [app.name], image: UIImage(systemName: app.icon)!) { _ in
                 self.launchUrlInCar(urlStr: app.url)
             }
@@ -38,33 +34,29 @@ class CarPlaySceneDelegate: UIResponder, CPTemplateApplicationSceneDelegate {
         }
         
         let gridTemplate = CPGridTemplate(title: "تطبيقات يزن الحقيقية", gridButtons: gridButtons)
-        self.interfaceController?.setRootTemplate(gridTemplate, animated: true)
+        // استخدام الطريقة المتوافقة لتجنب التحذير
+        self.interfaceController?.setRootTemplate(gridTemplate, animated: true, completion: nil)
     }
     
     func launchUrlInCar(urlStr: String) {
         guard let window = carWindow else { return }
         
-        // تنظيف الشاشة من أي متصفح قديم
         carWebView?.removeFromSuperview()
         window.subviews.forEach { $0.removeFromSuperview() }
         
-        // إعداد المتصفح الحقيقي للسيارة مع تخطي قيود أبل للميديا
         let config = WKWebViewConfiguration()
         config.allowsInlineMediaPlayback = true
         config.mediaTypesRequiringUserActionForPlayback = []
         
-        // إجبار المتصفح على اتخاذ حجم شاشة السيارة بالكامل
         let webViewFrame = CGRect(x: 0, y: 0, width: window.frame.width, height: window.frame.height)
         carWebView = WKWebView(frame: webViewFrame, configuration: config)
         
-        // تغيير الـ User Agent ليوهم المواقع أنه جهاز iPad حقيقي عشان تفتح النسخة الكاملة للموقع
         carWebView?.customUserAgent = "Mozilla/5.0 (iPad; CPU OS 17_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Mobile/15E148 Safari/604.1"
         
         if let web = carWebView {
             window.addSubview(web)
             web.load(URLRequest(url: URL(string: urlStr)!))
             
-            // زر الهوم العائم للرجوع لقائمة التطبيقات
             let homeBtn = UIButton(frame: CGRect(x: 15, y: 15, width: 45, height: 45))
             homeBtn.setImage(UIImage(systemName: "house.circle.fill"), for: .normal)
             homeBtn.tintColor = .green
@@ -76,13 +68,13 @@ class CarPlaySceneDelegate: UIResponder, CPTemplateApplicationSceneDelegate {
     }
     
     @objc func goBackToGrid() {
-        // حذف المتصفح والرجوع للقائمة الأساسية
         carWebView?.removeFromSuperview()
         carWindow?.subviews.forEach { $0.removeFromSuperview() }
         showAppGrid()
     }
     
-    func templateApplicationScene(_ templateApplicationScene: CPTemplateApplicationScene, didDisconnect interfaceController: CPInterfaceController) {
+    // تصحيح المسمى الرسمي لتجنب خطأ السطر 84
+    func templateApplicationScene(_ templateApplicationScene: CPTemplateApplicationScene, didDisconnectFrom interfaceController: CPInterfaceController) {
         self.interfaceController = nil
         self.carWindow = nil
     }
