d78f7cf91d50d73afd5b327e911ad40a6595a78c
Aln-gp <alnofaie.gp@gmail.com>
2026-06-30 17:27:36 +0300

    Refactor CarPlay app grid and add refresh button
    Removed hardcoded app list and added refresh button functionality in the CarPlay interface.

--- CarPlaySceneDelegate.swift
+++ CarPlaySceneDelegate.swift
@@ -8,72 +8,47 @@ class CarPlaySceneDelegate: UIResponder, CPTemplateApplicationSceneDelegate {
     var carWindow: CPWindow?
     var carWebView: WKWebView?
     
-    let allApps = [
-        CarAppItem(id: "yt", name: "YouTube", url: "https://www.youtube.com", icon: "play.rectangle.fill"),
-        CarAppItem(id: "tt", name: "TikTok", url: "https://www.tiktok.com", icon: "video.circle.fill"),
-        CarAppItem(id: "go", name: "Google", url: "https://www.google.com", icon: "magnifyingglass"),
-        CarAppItem(id: "tw", name: "X / Twitter", url: "https://www.x.com", icon: "message.fill")
-    ]
-    
+    // الدالة الرسمية التي تطلقها السيارة عند الاتصال
     func templateApplicationScene(_ templateApplicationScene: CPTemplateApplicationScene, didConnect interfaceController: CPInterfaceController, to window: CPWindow) {
         self.interfaceController = interfaceController
         self.carWindow = window
-        showAppGrid()
-    }
-    
-    func showAppGrid() {
-        let savedIDs = UserDefaults.standard.stringArray(forKey: "CarPlayEnabledApps") ?? ["yt", "tt", "go", "tw"]
-        let activeApps = allApps.filter { savedIDs.contains($0.id) }
         
-        var gridButtons: [CPGridButton] = []
-        for app in activeApps {
-            let btn = CPGridButton(titleVariants: [app.name], image: UIImage(systemName: app.icon)!) { _ in
-                self.launchUrlInCar(urlStr: app.url)
-            }
-            gridButtons.append(btn)
-        }
-        
-        let gridTemplate = CPGridTemplate(title: "تطبيقات يزن الحقيقية", gridButtons: gridButtons)
-        // استخدام الطريقة المتوافقة لتجنب التحذير
-        self.interfaceController?.setRootTemplate(gridTemplate, animated: true, completion: nil)
-    }
-    
-    func launchUrlInCar(urlStr: String) {
-        guard let window = carWindow else { return }
-        
-        carWebView?.removeFromSuperview()
-        window.subviews.forEach { $0.removeFromSuperview() }
-        
+        // إعداد المتصفح الحقيقي للسيارة مع تخطي قيود أبل للميديا
         let config = WKWebViewConfiguration()
         config.allowsInlineMediaPlayback = true
         config.mediaTypesRequiringUserActionForPlayback = []
         
+        // إجبار المتصفح على اتخاذ حجم شاشة السيارة بالكامل
         let webViewFrame = CGRect(x: 0, y: 0, width: window.frame.width, height: window.frame.height)
         carWebView = WKWebView(frame: webViewFrame, configuration: config)
         
+        // تغيير الـ User Agent ليوهم المواقع أنه جهاز iPad حقيقي عشان تفتح النسخة الكاملة للموقع وتشتغل الفيديوهات
         carWebView?.customUserAgent = "Mozilla/5.0 (iPad; CPU OS 17_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Mobile/15E148 Safari/604.1"
         
         if let web = carWebView {
             window.addSubview(web)
-            web.load(URLRequest(url: URL(string: urlStr)!))
+            // فتح محرك بحث جوجل كصفحة رئيسية ومنها تنطلق لأي موقع
+            web.load(URLRequest(url: URL(string: "https://www.google.com")!))
             
-            let homeBtn = UIButton(frame: CGRect(x: 15, y: 15, width: 45, height: 45))
-            homeBtn.setImage(UIImage(systemName: "house.circle.fill"), for: .normal)
-            homeBtn.tintColor = .green
-            homeBtn.backgroundColor = UIColor.black.withAlphaComponent(0.6)
-            homeBtn.layer.cornerRadius = 22.5
-            homeBtn.addTarget(self, action: #selector(goBackToGrid), for: .touchUpInside)
-            window.addSubview(homeBtn)
+            // زر تحديث الصفحة (Refresh) عائم فوق المتصفح في حال علّق الاتصال
+            let refreshBtn = UIButton(frame: CGRect(x: 15, y: 15, width: 45, height: 45))
+            refreshBtn.setImage(UIImage(systemName: "arrow.clockwise.circle.fill"), for: .normal)
+            refreshBtn.tintColor = .green
+            refreshBtn.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+            refreshBtn.layer.cornerRadius = 22.5
+            refreshBtn.addTarget(self, action: #selector(refreshPage), for: .touchUpInside)
+            window.addSubview(refreshBtn)
         }
+        
+        // إنشاء واجهة فارغة في الخلفية لتلبية متطلبات النظام
+        let template = CPBlankTemplate()
+        self.interfaceController?.setRootTemplate(template, animated: false, completion: nil)
     }
     
-    @objc func goBackToGrid() {
-        carWebView?.removeFromSuperview()
-        carWindow?.subviews.forEach { $0.removeFromSuperview() }
-        showAppGrid()
+    @objc func refreshPage() {
+        carWebView?.reload()
     }
     
-    // تصحيح المسمى الرسمي لتجنب خطأ السطر 84
     func templateApplicationScene(_ templateApplicationScene: CPTemplateApplicationScene, didDisconnectFrom interfaceController: CPInterfaceController) {
         self.interfaceController = nil
         self.carWindow = nil
